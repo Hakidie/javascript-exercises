1dd832a67edd05548711a000169fd09b793fc118
Hakidie <99023101+Hakidie@users.noreply.github.com>
2025-12-10 15:46:46 +0700

    finished EX08

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(inputString) {
+    let outputString = "";
+    let stringLength = inputString.length - 1;
 
+    for(let i = stringLength; i >= 0; i--) {
+        outputString += inputString.at(i);
+    }
+
+    return outputString;
 };
 
 // Do not edit below this line
--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...inputValue) {
+    const setOfRemovedValues = new Set(inputValue);
+    //const outputArray = inputArray.filter(element => element != inputValue1);
+    const outputArray = inputArray.filter(element => {
+        return !setOfRemovedValues.has(element)
+    });
+    return outputArray;
 };
 
 // Do not edit below this line
